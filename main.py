5adea0f898a950dea1caf800cfb05f3690da0743
Paulo Ricardo <84928987+paulotobias@users.noreply.github.com>
2026-07-03 16:29:35 +0000

    Adicionado listar

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from datetime import datetime
 
+LISTA_TAREFAS = []
 APP = FastAPI()
 
+def nova_tarefa(id: int, titulo: str, descricao: str):
+    return{
+        "id": id,
+        "titulo": titulo,
+        "descricao": descricao,
+        "concluido": False,
+        "criado_em": datatime.now()
+    }
 @APP.get("/")
 def index():
     return "Olá, Devops!"
+
+@APP.get("/tarefas")
+def lista_tarefas():
+    if len(LISTA_TAREFAS) == 0:
+        return LISTA_TAREFAS
+
+    tarefas = []
+    for tarefa in LISTA_TAREFAS:
+        info = {"id": tarefa['id'], "titulo": tarefa['titulo']}
+        tarefas.append(info)
+
+    return tarefas
